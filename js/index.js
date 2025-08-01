704ff50b7af1aa2bcbea769842897ceb20c16e40
Anas-Abuhamed <anasabuhamed07@gmail.com>
2025-08-01 23:44:40 +0300

    implement required features (student CRUD, statistics); optional tasks not included

--- js/index.js
+++ js/index.js
@@ -1,14 +1,19 @@
 const popup = document.getElementById("student-popup");
 const overlay = document.querySelector(".overlay");
 const searchInput = document.getElementById("search-input");
-const form = document.querySelector(".popup-form form");
+const popupForm = document.querySelector(".popup-form")
+const form = popupForm.querySelector("form");
 const cancelBtn = document.getElementById("cancel-student");
 const addStudentBtn = document.getElementById("open-popup");
 let students = getStudentsFromLocalStorage();
+// global btns
 let editBtns;
 let deleteBtns;
+// global edit informations
 let isEditing = false;
 let editIndex = null;
+let updatingElement;
+// display students for 1st time
 displayStudents();
 // Open add student popup
 function showPopup() {
@@ -22,21 +27,31 @@ function hidePopup() {
     overlay.classList.add("hidden");
     form.reset();
     isEditing = false;
-    editIndex = null
+    editIndex = null;
 }
-
+popupForm.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        form.requestSubmit();
+    } else if (e.key === "Escape") {
+        hidePopup();
+    }
+})
 cancelBtn.addEventListener("click", hidePopup);
 overlay.addEventListener("click", hidePopup);
 addStudentBtn.addEventListener("click", showPopup);
 
 // Search functionality
-searchInput.addEventListener(("input"), () => {
+searchInput.addEventListener("input", () => {
     search();
-})
+});
 function search() {
     const studentsName = document.querySelectorAll("tbody tr td.name");
     studentsName.forEach((name) => {
-        name.parentElement.style.display = name.textContent.toLowerCase().includes(searchInput.value.trim().toLowerCase()) ? "" : "none"
+        name.parentElement.style.display = name.textContent
+            .toLowerCase()
+            .includes(searchInput.value.trim().toLowerCase())
+            ? ""
+            : "none";
     });
 }
 
@@ -46,9 +61,11 @@ form.addEventListener("submit", (e) => {
     const name = document.getElementById("student-name").value.trim();
     const gpa = parseFloat(document.getElementById("student-gpa").value.trim());
     const major = document.getElementById("student-major").value.trim();
-    const isUpdatedNameExist = students.findIndex(student => student.name === name)
+    const isUpdatedNameExist = students.findIndex(
+        (student) => student.name === name
+    );
     if (isUpdatedNameExist != -1 && isUpdatedNameExist != editIndex) {
-        alert("name already exist !")
+        alert("name already exist !");
         return;
     }
     if (isNaN(gpa) || gpa < 0 || gpa > 4) {
@@ -56,16 +73,27 @@ form.addEventListener("submit", (e) => {
         return;
     }
     if (isEditing) {
+        const indexToEdit = editIndex;
+        hidePopup();
         const editedStudent = {
-            ...students[editIndex],
+            ...students[indexToEdit],
             name,
             gpa,
-            major
-        }
-        students[editIndex] = editedStudent;
-    }
-    else {
-        const isNameExist = students.findIndex(student => student.name === name);
+            major,
+        };
+        showConfirmPopup("Are you sure want to edit this student's information ?", (confirm) => {
+            if (confirm) {
+                students[indexToEdit] = editedStudent;
+                saveStudentsToLocalStorage();
+                displayStudents();
+                updatingElement = null
+            }
+            else {
+                editStudent(updatingElement)
+            }
+        })
+    } else {
+        const isNameExist = students.findIndex((student) => student.name === name);
         if (isNameExist !== -1) {
             alert("Name already exists!");
             return;
@@ -75,36 +103,15 @@ form.addEventListener("submit", (e) => {
             id: Date.now(),
             name,
             gpa,
-            major
+            major,
         };
         students.push(newStudent);
+        saveStudentsToLocalStorage();
+        displayStudents();
+        hidePopup();
     }
-    saveStudentsToLocalStorage();
-    displayStudents();
-    hidePopup();
-})
-function createStudent(e) {
-    e.preventDefault();
-    const gpa = parseFloat(document.getElementById("student-gpa").value.trim());
-    if (isNaN(gpa) || gpa < 0 || gpa > 4) {
-        alert("Please enter a valid GPA between 0 and 4.");
-        return;
-    }
-    const name = document.getElementById("student-name").value.trim();
-    const major = document.getElementById("student-major").value.trim();
-    const isNameExist = students.findIndex(student => student.name == name);
-    if (isNameExist != -1) {
-        alert("name already exist !")
-        return;
-    }
-    const newStudent = {
-        id: Date.now(),
-        name,
-        gpa,
-        major
-    }
-    addStudent(newStudent);
-}
+
+});
 
 // Display students in the table
 function displayStudents() {
@@ -124,43 +131,62 @@ function displayStudents() {
             </td>
         `;
         tableBody.appendChild(row);
-        form.reset();
         // Add event listeners for delete button
         const deleteBtn = row.querySelector(".delete");
-        deleteBtn.addEventListener("click", deleteStudent);
+        // deleteBtn.addEventListener("click", deleteStudent);
+        deleteBtn.addEventListener("click", (e) => {
+            showConfirmPopup("Are you sure want to delete this student ?", (confirm) => deleteStudent(e, confirm))
+        }
+        );
         // Add event listeners for edit button
         const editBtn = row.querySelector(".edit");
         editBtn.addEventListener("click", editStudent);
     });
+    showStatistics();
+}
+// show Statistics
+function showStatistics() {
+    const total = students.length;
+    const avgGPA = total === 0 ? 0.00 : (students.reduce((acc, cur) => acc + cur.gpa, 0) / total).toFixed(2);
+    const majorCount = {};
+    students.forEach(student => {
+        majorCount[student.major] = (majorCount[student.major] || 0) + 1;
+    })
+    document.querySelector(".students-number span").textContent = total;
+    document.querySelector(".avg-gpa span").textContent = avgGPA;
+    document.querySelector(".major-count").innerHTML = ""
+    for (const major in majorCount) {
+        document.querySelector(".major-count").innerHTML += `<p>${major} : ${majorCount[major]}</p>`
+    }
 }
 
 // Edit student information functionality
 function editStudent(e) {
+    updatingElement = e;
     const row = e.target.closest("tr");
     const id = row.getAttribute("data-id");
-    const studentIndex = students.findIndex(student => student.id == id)
+    const studentIndex = students.findIndex((student) => student.id == id);
     const student = students[studentIndex];
 
     document.getElementById("student-name").value = student.name;
     document.getElementById("student-gpa").value = student.gpa.toFixed(2);
     document.getElementById("student-major").value = student.major;
     isEditing = true;
-    editIndex = studentIndex
+    editIndex = studentIndex;
     showPopup();
 }
 
 // Delete student functionality
-function deleteStudent(e) {
-    const row = e.target.closest("tr");
-    const id = row.getAttribute("data-id");
-    students = students.filter(student => student.id != id);
-    row.remove();
-    saveStudentsToLocalStorage();
-    displayStudents();
+function deleteStudent(e, confirmed) {
+    if (confirmed) {
+        const row = e.target.closest("tr");
+        const id = row.getAttribute("data-id");
+        students = students.filter((student) => student.id != id);
+        row.remove();
+        saveStudentsToLocalStorage();
+        showStatistics()
+    }
 }
-
-
-
 
 // Get and set students in local storage
 
@@ -170,4 +196,44 @@ function getStudentsFromLocalStorage() {
 }
 function saveStudentsToLocalStorage() {
     localStorage.setItem("students", JSON.stringify(students));
+}
+
+// confirmation popup
+function showConfirmPopup(message, callback) {
+    // popup for add & delete confirmation
+    const popup = document.querySelector(".confirm-popup");
+    popup.innerHTML = `
+    <div class="confirm-popup-overlay">
+        <div class="confirm-popup-content">
+        <p>${message}</p>
+        <button class="cancel btn">Cancel</button>
+        <button class="confirm btn">Confirm</button>
+    </div>
+    </div>
+    `;
+    popup.classList.add("show");
+
+    const cancelBtn = popup.querySelector(".cancel");
+    const confirmBtn = popup.querySelector(".confirm");
+
+    function close(result) {
+        // To close popup
+        popup.classList.remove("show");
+        document.removeEventListener("keydown", handleKey);
+        callback(result);
+    }
+
+    cancelBtn.onclick = () => close(false);
+    confirmBtn.onclick = () => close(true);
+
+    function handleKey(e) {
+        // for key actions
+        if (e.key === "Enter") {
+            close(true);
+        } else if (e.key === "Escape") {
+            close(false);
+        }
+    }
+
+    document.addEventListener("keydown", handleKey);
 }
